0da6c5b9f0af59e47f0385dba5c5a2f113d0ff48
Duc-Thomas <duckiduc.dev@gmail.com>
2021-10-23 17:45:44 +0200

    Cta

---
+++ src/components/CtaCustomize.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="third-section">
+    <div class="cta d-flex cta-customize">
+      <div class="text">
+        <h2>Customize your VPS to fit your needs</h2>
+        <p>With our VPS configuration tool</p>
+      </div>
+      <div class="button">
+        <button class="letsgo">Let's go !</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "CtaCustomize",
+});
+</script>
+<style lang="scss" scoped>
+.cta {
+  background-color: white;
+  box-shadow: 0px 1px 25px rgba(200, 197, 197, 0.25);
+  padding: 34px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  border-radius: 5px;
+}
+.button {
+  display: flex;
+  align-items: center;
+}
+.letsgo {
+  background-color: #d3208b;
+  color: white;
+  padding: 10px 50px 10px 50px;
+  border: none;
+  border-radius: 5px;
+}
+
+.third-section {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 30px 0px 30px 0px;
+  padding: 0px 20px 0px 20px;
+}
+.cta-customize {
+  width: 100%;
+  max-width: 1200px;
+}
+</style>
